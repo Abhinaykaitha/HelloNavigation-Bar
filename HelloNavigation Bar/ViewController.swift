8508199e1a3f8c57792af2e28b775a1828aa5262
Kaitha,Abhinay Reddy <s534051@ch140015m.nwmissouri.edu>
2019-02-21 13:47:38 -0600

    Initial Commit

---
+++ HelloNavigation Bar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloNavigation Bar
+//
+//  Created by Kaitha,Abhinay Reddy on 2/21/19.
+//  Copyright © 2019 Kaitha,Abhinay Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
